d50ceeca89640440cc18cc8bba80c0bff2af5712
sMathujan <sivananthanmathujan@gmail.com>
2024-07-06 08:50:11 +0530

    Setup tasks has been updated successfully.

--- crewai_be/crew.py
+++ crewai_be/crew.py
@@ -1,5 +1,7 @@
 from job_manager import append_event
 from agents import CompanyResearchAgents
+from tasks import CompanyResearchTasks
+from crewai import Crew
 
 
 class CompanyResearchCrew:
@@ -9,16 +11,32 @@ class CompanyResearchCrew:
 
     def setup_crew(self, companies: list[str], positions: list[str]):
         
+        # SETUP AGENTS
         agents = CompanyResearchAgents()
-
         research_manager = agents.research_manager(
             companies, positions
         )
         company_research_agent = agents.company_research_agent()
 
 
-        # TODO: SETUP TASKS
-        # TODO: CREATE CREW
+        # SETUP TASKS
+        tasks = CompanyResearchTasks(
+            job_id=self.job_id
+        )
+        company_research_tasks = [
+            tasks.company_research(company_research_agent, company, positions) for company in companies
+        ]
+        manage_research_task = tasks.manage_research(
+            research_manager, companies, positions, company_research_tasks
+        )
+
+        # CREATE CREW
+        self.crew = Crew(
+            agents=[research_manager, company_research_agent],
+            tasks=[*company_research_tasks, manage_research_task],
+            verbose=2,
+        )
+
 
     def kickoff(self):
         if not self.crew:
